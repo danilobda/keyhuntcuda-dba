0d1df6c39cc34dfaa67f1783bcbbfbe530ad4f82
Thiago <90641450+vkThiago@users.noreply.github.com>
2024-10-09 22:49:54 -0300

    Update sha256.h

--- hash/sha256.h
+++ hash/sha256.h
@@ -15,11 +15,11 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <cstdint>
+
 #ifndef SHA256_H
 #define SHA256_H
 #include <string>
-#include <cstdint>
-
 
 void sha256(uint8_t *input, int length, uint8_t *digest);
 void sha256_33(uint8_t *input, uint8_t *digest);
@@ -33,5 +33,3 @@ void sha256sse_checksum(uint32_t *i0, uint32_t *i1, uint32_t *i2, uint32_t *i3,
                         uint8_t *d0, uint8_t *d1, uint8_t *d2, uint8_t *d3);
 std::string sha256_hex(unsigned char *digest);
 void sha256sse_test();
-
-#endif
